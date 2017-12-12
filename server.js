74d4dd04eaa1a6741769598faece5e7be3073caf
Marine Petit <petitm31@gmail.com>
2017-12-12 17:28:12 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server is now listening on port ${port}`);
+});
+
+module.exports = server;
