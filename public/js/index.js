c96a9ec90afb0be3e2b1cd105fbd9b0fc4436d4b
Marine Petit <petitm31@gmail.com>
2017-12-13 12:40:07 +0100

    Response added

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,31 @@
-const client = new ApiAi.ApiAiClient({
-  accessToken: 'cda0fc3c3b8b4834a89b940d30106337'
-});
+// const apiAi = require('./ApiAi');
 
-// recupérer du DOM la requête
+// const client = new apiAi.ApiAiClient({
+//   accessToken: 'cda0fc3c3b8b4834a89b940d30106337'
+// });
 
-// let request = ;
-// client.textRequest(request)
-// .then(handleResponse)
-// .catch(handleError);
+// // recupérer du DOM la requête
+// function getRequest() {
+//   let request = document.getElementById('request').value;
+//   request.addEventListener('click', function (event) {
+//     client.textRequest(request)
+//       .then(handleResponse)
+//       .catch(handleError);
+//   });
+// }
 
-function handleResponse(serverResponse) {
-  console.log(serverResponse);
-}
+// function handleResponse(serverResponse) {
+//   return res.json({
+//     fulfillment: {
+//       speech: `Sushi Shop vous propose une recette d'oeuf cocotte à la moutarde`,
+//       displayText: `Sushi Shop vous propose une recette d'oeuf cocotte à la moutarde`,
+//       source: `webhook`
+//     }
+//   });
+// }
 
-function handleError(serverError) {
-  console.log(serverError);
-}
+// function handleError(serverError) {
+//   console.log(serverError);
+// }
+
+// module.exports = getRequest;
--- server.js
+++ server.js
@@ -19,21 +19,45 @@ app.get('/', function (req, res, next) {
 });
 
 app.post('/webhook', function (req, res, next) {
-  let ingredient = req.body.result.resolvedQuery;
-  return res.json({
-    fulfillment: {
-      speech: `Sushi Shop vous propose une recette d'oeuf cocotte à la moutarde`,
-      displayText: `Sushi Shop vous propose une recette d'oeuf cocotte à la moutarde`,
-      source: `webhook`
+    let request = req.body.result.resolvedQuery;
+    if (request.includes('oeufs')) {
+      return res.json({
+        speech: `Bonjour Céline, Sushi Shop vous propose une recette d'oeuf cocotte à la moutarde (Temps: 10 minutes de préparation, 10 minutes de cuisson). Voulez-vous faire la recette ?`,
+        displayText: `Bonjour Céline, Sushi Shop vous propose une recette d'oeuf cocotte à la moutarde (Temps: 10 minutes de préparation, 10 minutes de cuisson). Voulez-vous faire la recette ?`,
+        source: `webhook`
+      });
+    } else if (request.includes('oui') || request.includes('je veux bien') || request.includes(`d'accord`)) {
+      return res.json({
+        speech: `Voici les étapes de la recette. Etape 1: Préchaffer le four à 180°. Mélanger la crème et la moutarde. La verser dans un ramequin.`,
+        displayText: `Voici les étapes de la recette. Etape 1: Préchaffer le four à 180°. Mélanger la crème et la moutarde. La verser dans un ramequin.`,
+        source: `webhook`
+      });
+    } else if (request.includes('étape')) {
+      return res.json({
+        speech: `Etape 2: Casser l'oeuf sur la crème. Saupoudrer le parmesan râpé, en évitant le jaune. Ajouter les petits morceaux de tomates sur le blanc d'oeuf. Saler légèrement et poivrer.`,
+        displayText: `Etape 2: Casser l'oeuf sur la crème. Saupoudrer le parmesan râpé, en évitant le jaune. Ajouter les petits morceaux de tomates sur le blanc d'oeuf. Saler légèrement et poivrer.`,
+        source: `webhook`
+      });
+    } else if (request.includes(`fait`)) {
+      return res.json({
+        speech: `Etape 3: Enfourner pendant 10 minutes environ, jusqu'à ce que le blanc ait pris et que le jaune soit encore liquide. Servir bien chaud, avec quelques copeaux de parmesan sur le dessus, accompagné de pain beurré.`,
+        displayText: `Etape 3: Enfourner pendant 10 minutes environ, jusqu'à ce que le blanc ait pris et que le jaune soit encore liquide. Servir bien chaud, avec quelques copeaux de parmesan sur le dessus, accompagné de pain beurré.`,
+        source: `webhook`
+      });
+    } else if (request.includes(`non`)) {
+      return res.json({
+        speech: `Voulez-vous commander sur Sushi Shop ?`,
+        displayText: `Voulez-vous commander sur Sushi Shop ?`,
+        source: `webhook`
+      });
+    } else if (request.includes(`menu`) || request.includes(`repas`)) {
+      return res.json({
+        speech: `Ce soir Sushi Shop vous propose de réaliser la recette du chef Jean-François Piége, Bowls de daurade aux agrumes`,
+        displayText: `Ce soir Sushi Shop vous propose de réaliser la recette du chef Jean-François Piége, Bowls de daurade aux agrumes`,
+        source: `webhook`,
+      });
     }
   });
-}, (error) => {
-  return res.json({
-    speech: 'Something went wrong!',
-    displayText: 'Something went wrong!',
-    source: 'webhook'
-  });
-});
 
 const server = app.listen(port, () => {
   console.log(`Server is now listening on port ${port}`);
